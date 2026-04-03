4ed54d7ea0df1b2dc73f5d3b6eb514a180dc5515
Omur Ozbahceliler <omur@rootifera.org>
2026-04-03 22:12:44 +0100

    icons

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ from __future__ import annotations
 import sys
 
 from PySide6.QtCore import QCoreApplication
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 
 from ui.main_window import MainWindow
 
 
 def main() -> int:
+    if sys.platform == "win32":
+        import ctypes
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            "sansarsam.app"
+        )
+
     app = QApplication(sys.argv)
+
+    app.setWindowIcon(QIcon("assets/icon.png"))
+
     QCoreApplication.setOrganizationName("Sansarsam")
     QCoreApplication.setApplicationName("Sansarsam")
+
     window = MainWindow()
+    window.setWindowIcon(QIcon("assets/icon.png"))
     window.show()
     return app.exec()
 
